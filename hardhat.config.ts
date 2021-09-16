4a0425f8d544cd5d11d8af7e26de71fc4234415c
wxn <wxn.sjtu@gmail.com>
2021-09-15 23:38:57 -0700

    remove namedaccounts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,11 +17,6 @@ const config: HardhatUserConfig = {
       evmVersion: 'istanbul',
     },
   },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    }
-  },
   networks: {
     hardhat: {
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
